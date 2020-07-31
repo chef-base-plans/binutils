fd07f1206fbe5af6cb48fc6ef6594dd77d673565
Steven Marshall <SMarshall@chef.io>
2020-07-31 11:39:06 +0100

    [binutils] Remove tmp from hab command
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- controls/binutils_works.rb
+++ controls/binutils_works.rb
@@ -2,7 +2,6 @@ title 'Tests to confirm binutils works as expected'
 
 plan_name = input('plan_name', value: 'binutils')
 plan_ident = "#{ENV['HAB_ORIGIN']}/#{plan_name}"
-hab_path = input('hab_path', value: '/tmp/hab')
 
 control 'core-plans-binutils' do
   impact 1.0
@@ -19,7 +18,7 @@ control 'core-plans-binutils' do
   We also need to ensure that the -dynamic-linker is set to the glibc ld-linux-x86_64.so.2 that this
   package was set with. To do that, we pull out the fully qualified ident for glibc from the DEPS.
   '
-  binutils_pkg_ident = command("#{hab_path} pkg path #{plan_ident}")
+  binutils_pkg_ident = command("hab pkg path #{plan_ident}")
   describe binutils_pkg_ident do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
